7c3ba8bf03399aeca307d80e598ecf28badc64a0
Alexander Paterson <alex1234@live.com.au>
2016-05-27 21:26:58 +0800

    fixed requirements.txt

--- tests/helpers.py
+++ tests/helpers.py
@@ -39,8 +39,6 @@ def solve_challenge(message, priv_key):
     decrypt = rsa_key.private_decrypt(message, RSA.pkcs1_padding)
     return int(decrypt) + int(499)
 
-
-
 def encrypt_solution(message, private_key_string):
     rsa_key = RSA.load_key_string(private_key_string)
     cipher = rsa_key.private_encrypt(message, RSA.pkcs1_padding)
