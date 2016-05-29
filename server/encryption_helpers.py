cd3af3dab3d043d2c863d46f63434f46b284d026
Simon de Sancha <simondesancha@gmail.com>
2016-05-29 16:04:07 +0800

    fixes

--- server/encryption_helpers.py
+++ server/encryption_helpers.py
@@ -1,7 +1,7 @@
 import base64
 from Crypto.Cipher import AES
 from M2Crypto import RSA
-
+from .helpers import ascii_to_length
 
 def encrypt_with_key(message, rsa_key):
     cipher = rsa_key.public_encrypt(message, RSA.pkcs1_padding)
@@ -10,7 +10,13 @@ def encrypt_with_key(message, rsa_key):
 def check_solution(message, original_number, rsa_key):
     message = base64.b64decode(message)
     decrypt = rsa_key.public_decrypt(message, RSA.pkcs1_padding)
-    solution = int(decrypt)
+
+    #This is needed to allow use with both python/java client:
+    if(len(decrypt) < 4):
+        solution = int(decrypt)
+    else:
+        solution = ascii_to_length(decrypt)
+
     if solution == int(original_number) + 499:
         return True
     else:
